9748e9617859fff78ab186ca077ed34d896df0e4
Aqib Malik <malik.ab454@gmail.com>
2022-06-27 12:31:55 +0500

    Initial Commit

--- Python/TwoSum.py
+++ Python/TwoSum.py
@@ -1,4 +1,5 @@
 '''
+    LEET-CODE #01 https://leetcode.com/problems/two-sum/
     Problem: Given an array of integers numbers and an integer target, 
              return indices of the two numbers such that they add up to target.
              You may assume that each input would have exactly one solution, 
