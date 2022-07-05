21daef77384fa0d58cf82e6fa23f4bf2b93a9eb2
Aqib Malik <malik.ab454@gmail.com>
2022-07-05 17:06:56 +0500

    Merge Sorted Array problem added

---
+++ Javascript/MergeSortedArray.js
@@ -0,0 +1,29 @@
+/** LEET-CODE #88
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+ var merge = function(nums1, m, nums2, n) {
+    let index = m + n - 1;
+    
+    while(m > 0 && n > 0){
+        if(nums2[n-1] >= nums1[m-1]){
+            nums1[index] = nums2[n-1];
+            nums2.pop();
+            n--;
+        }else{
+            let temp = nums1[m-1]
+            nums1[m-1] = nums1[index]
+            nums1[index] = temp;
+            m--;
+        }
+        index--;
+    }
+    while(nums2.length > 0){
+        let len = nums2.length-1;
+        nums1[len] = nums2[len];
+        nums2.pop();
+    }
+};
